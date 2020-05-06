d6e663e9fe3ece1ca4ecaf789c65dd9b01fb675d
tianjingyu1 <820410740@qq.com>
2020-05-06 10:10:26 +0800

    二叉树的链表结构

---
+++ binaryTree-linked/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+typedef struct TNode *Position;
+typedef Position BinTree; /* 二叉树类型 */
+struct TNode{ /* 树结点定义 */
+    ElementType Data; /* 结点数据 */
+    BinTree Left;     /* 指向左子树 */
+    BinTree Right;    /* 指向右子树 */
+};
+
+int main(int argc, char** argv) {
+	return 0;
+}
